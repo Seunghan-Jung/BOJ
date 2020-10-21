deb39b4f683266103850afc62aa1df49f4902234
cool-hammer <ajtwlstmdgks@gmail.com>
2020-10-22 01:21:54 +0900

    수영장 만들기 그래프, 노드 방식으로 풀이 완료

--- 1113/수영장 만들기.py
+++ 1113/수영장 만들기.py
@@ -1,73 +1,92 @@
-# import sys
-#
-# sys.stdin = open('input.txt')
-#
-#
-# class Ground:
-#
-#     def __init__(self, height):
-#         self.size = 1
-#         self.height = height
-#         self.adj = set()
-#         self.available = True
-#
-#
-# N, M = map(int, input().split())
-#
-# grid = [[*map(int, input())] for _ in range(N)]
-#
-# pools = []
-#
-# delta = ((1, 0), (0, 1), (-1, 0), (0, -1))
-#
-#
-# def dfs(x, y, ground: Ground):
-#     for dx, dy in delta:
-#         nx, ny = x + dx, y + dy
-#
-#         if nx < 0 or nx >= N or ny < 0 or ny >= M:
-#             ground.adj.add(plain)
-#             continue
-#
-#         if grid[nx][ny] == ground.height:
-#             _ground = where[nx][ny]
-#
-#             if _ground is plain:
-#                 where[nx][ny] = ground
-#                 ground.size += 1
-#                 dfs(nx, ny, ground)
-#
-#             elif _ground != ground:
-#                 ground.size += _ground.size
-#                 ground.adj.update(_ground.adj)
-#                 G.remove(_ground)
-#         else:
-#             _ground = where[nx][ny]
-#
-#             if _ground is plain:
-#                 create(nx, ny, ground)
-#
-#             else:
-#                 ground.adj.add(_ground)
-#                 _ground.adj.add(ground)
-#
-#
-# def create(x, y, pre):
-#     height = grid[x][y]
-#     new = Ground(height)
-#     pre.adj.add(new)
-#     new.adj.add(pre)
-#
-#     G.add(new)
-#
-#     dfs(x, y, new)
-#
-#
-# G = set()
-# inf = float('inf')
-# plain = Ground(inf)
-#
-# where = [[plain] * M for _ in range(N)]
-# create(0, 0, plain)
-#
-# print(G)
+import sys
+
+sys.stdin = open('input.txt')
+
+sys.setrecursionlimit(10 ** 4)
+
+
+class Node:
+
+    def __init__(self, height):
+        self.size = 1
+        self.height = height
+        self.adj = set()
+
+    # def __repr__(self):
+    #     return f'높이 {self.height} 너비 {self.size}'
+
+
+N, M = map(int, input().split())
+
+grid = [[*map(int, input())] for _ in range(N)]
+
+where = [[None] * M for _ in range(N)]
+
+delta = ((-1, 0), (0, 1), (1, 0), (0, -1))
+
+
+def check(x, y):
+    return 0 <= x < N and 0 <= y < M
+
+
+def dfs(x, y, node):
+    for dx, dy in delta:
+        nx, ny = x + dx, y + dy
+
+        if not check(nx, ny):
+            node.adj.add(ground)
+            ground.adj.add(node)
+            node_list.discard(node)
+            continue
+
+        if grid[nx][ny] == node.height:
+            if where[nx][ny] is None:
+                node.size += 1
+                where[nx][ny] = node
+                dfs(nx, ny, node)
+        elif where[nx][ny] is not None:
+            node.adj.add(where[nx][ny])
+            where[nx][ny].adj.add(node)
+
+
+def create(x, y):
+    new_node = Node(grid[x][y])
+    node_list.add(new_node)
+    where[x][y] = new_node
+    dfs(x, y, new_node)
+
+
+ground = Node(float('-inf'))
+node_list = set()
+
+for i in range(N):
+    for j in range(M):
+        if where[i][j] is None:
+            create(i, j)
+
+answer = 0
+
+while node_list:
+    node = min(node_list, key=lambda n: n.height)
+    min_node = min(node.adj, key=lambda n: n.height)
+
+    if min_node.height < node.height:
+        node_list.remove(node)
+        continue
+    answer += (min_node.height - node.height) * node.size
+
+    node.adj.update(min_node.adj)
+    node.size += min_node.size
+    node.height = min_node.height
+
+    for n in min_node.adj:
+        n.adj.remove(min_node)
+        n.adj.add(node)
+
+    node.adj.remove(node)
+    if min_node in node_list:
+        node_list.remove(min_node)
+    else:
+        node_list.remove(node)
+
+print(answer)
